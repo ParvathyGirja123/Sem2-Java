f60826205cd7d17c703a35a82b01984b7e37204c
Parvathy <117814878+ParvathyGirja123@users.noreply.github.com>
2023-03-13 15:20:31 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Even
+{
+public static void main(String args[])
+{
+int num;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number: ");
+num=sc.nextInt();
+if(num%2==0)
+System.out.println("Number is even");
+else
+System.out.println("Number is odd");
+}
+}	
