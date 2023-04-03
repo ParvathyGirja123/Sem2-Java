4ad68c4e9c56028939ec7d1fe9609ba2956df945
Parvathy <117814878+ParvathyGirja123@users.noreply.github.com>
2023-04-03 15:46:33 +0530

    Update 10-Stringsort.java

--- 10-Stringsort.java
+++ 10-Stringsort.java
@@ -1,23 +1,12 @@
-import java.util.Arrays;  
-public class stringsort 
-{  
-public static void main(String args[])   
-{   
-String[] message = {"hii","welcome","to","java","lab"};  
-int size = message.length;  
- 
-for(int i = 0; i<size-1; i++)   
-{  
-for (int j = i+1; j<message.length; j++)   
-{    
-if(message[i].compareTo(message[j])>0)   
-{    
-String temp = message[i];  
-message[i] = message[j];  
-message[j] = temp;  
-}  
-}  
-}    
-System.out.println(Arrays.toString(message));  
-}  
-} 
+import java.util.Arrays;
+import java.util.Scanner;
+public class SortingString {
+   public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a string value: ");
+      String str = sc.nextLine();
+      char charArray[] = str.toCharArray();
+      Arrays.sort(charArray);
+      System.out.println(new String(charArray));
+   }
+}
