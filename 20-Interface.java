24b26cb81a519078b68f51447c754814503ed813
Parvathy <117814878+ParvathyGirja123@users.noreply.github.com>
2023-06-19 13:57:01 +0530

    Update 20-Interface.java

--- 20-Interface.java
+++ 20-Interface.java
@@ -1,14 +1,9 @@
-class Students {
-    private String name;
-    private int academicScore;
+// Parent class - Student
+class Student {
+    protected int academicScore;
 
-    public Students(String name, int academicScore) {
-        this.name = name;
+    public Student(int academicScore) {
         this.academicScore = academicScore;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public int getAcademicScore() {
@@ -16,8 +11,9 @@ class Students {
     }
 }
 
+// Parent class - Sports
 class Sports {
-    private int sportsScore;
+    protected int sportsScore;
 
     public Sports(int sportsScore) {
         this.sportsScore = sportsScore;
@@ -28,22 +24,26 @@ class Sports {
     }
 }
 
-class Result extends Students, Sports{
-    public Result(String name, int academicScore, int sportsScore) {
-        super(name, academicScore);
+// Child class - Result
+class Result extends Student, Sports {
+    public Result(int academicScore, int sportsScore) {
+        super(academicScore);
         this.sportsScore = sportsScore;
     }
 
     public void displayScores() {
-        System.out.println("Name: " + getName());
         System.out.println("Academic Score: " + getAcademicScore());
         System.out.println("Sports Score: " + getSportsScore());
     }
 }
 
+// Main class
 public class Main {
     public static void main(String[] args) {
-        Result studentResult = new Result("John Doe", 85, 90);
-        studentResult.displayScores();
+        // Create an instance of Result
+        Result result = new Result(90, 85);
+
+        // Display the scores
+        result.displayScores();
     }
 }
